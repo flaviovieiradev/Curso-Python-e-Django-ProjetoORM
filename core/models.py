bfaaeb87897529ae477b87cd2f63d65317ad80dd
flaviovieiradev <analyst.flaviovieira@gmail.com>
2024-08-12 11:48:22 -0300

    Término da aula Relacionamento Um para Um

--- core/models.py
+++ core/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Chassi(models.Model):
-    numero = models.CharField('Chassi', max_length=17)
+    numero = models.CharField('Chassi', max_length=17, help_text='Máximo 16 caracteres')
 
     class Meta:
         verbose_name = 'Chassi'
@@ -18,7 +18,7 @@ class Carro(models.Model):
         chassi só pode se relacionar com um carro.
     """
     chassi = models.OneToOneField(Chassi, on_delete=models.CASCADE)
-    modelo = models.CharField('Modelo', max_length=30)
+    modelo = models.CharField('Modelo', max_length=30, help_text='Máximo 30 caracteres')
     preco = models.DecimalField('Preço', max_digits=8, decimal_places=2)
 
     class Meta:
